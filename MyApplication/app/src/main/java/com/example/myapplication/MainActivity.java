768e2e5e9e3fb67df49de597b92e425467d8c243
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2020-10-10 21:23:17 -0300

    2

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,31 +10,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText editext;
     Button favorito;
+    private List<Contacto> contactos;
+    private RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Object contactos = new ArrayList<Contacto>();
-        ((ArrayList) contactos).add(new Contacto(R.drawable.ic_launcher_background, "Pablo","0789","pablo@ejemplo"));
-        editext = findViewById(R.id.Nombre);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
     }
 
-    editext.setString(getIntent().getStringExtra("Nombre"));
+    initializeData();
+    initializeAdapter();
 
-  favorito.setOnClickListener(new View.OnClickListener()
-    private final Object v;
 
-    {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, Favoritos.class);
-            intent.putExtra("InfoNombre",editext.getText().toString());
-            startActivity(intent);
-        }
-    });
+    private void initializeData() {
+        contactos = new ArrayList<>();
+        contactos.add(new Contacto(R.drawable.descarga1,"Pluto", "6", ));
+        contactos.add(new Contacto(R.drawable.descarga2,"Guau", "6", ));
+        contactos.add(new Contacto(R.drawable.descarga3,"Oliver", "6", ));
+        contactos.add(new Contacto(R.drawable.descarga4,"Alex", "6", ));
+
+    }
+
+    private void initializeAdapter() {
+        ContactoAdaptador adapter = new ContactoAdaptador((ArrayList<Contacto>) contactos);
+        rv.setAdapter(adapter);
+    }
 }
